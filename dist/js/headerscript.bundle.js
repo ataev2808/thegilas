893070eed2674e0eefc88a158e47295fc1c80feb
thegilasband <ataev2808@mail.ru>
2024-08-18 00:19:38 +0300

    Update headerscript.bundle.js

--- dist/js/headerscript.bundle.js
+++ dist/js/headerscript.bundle.js
@@ -1,1 +1,50 @@
-(()=>{"use strict";const e=document.querySelector(".header-burger"),t=document.querySelector(".menu-body");e&&e.addEventListener("click",(function(o){document.body.classList.toggle("_lock"),e.classList.toggle("_active"),t.classList.toggle("_active")}));const o=document.querySelectorAll(".menu-link[data-goto]");if(o.length>0){function c(o){const c=o.target.dataset.goto;if(c){if("/"===window.location.pathname||window.location.pathname.endsWith("/index.html")){if(document.querySelector(c)){const o=document.querySelector(c).getBoundingClientRect().top+window.scrollY-document.querySelector("header").offsetHeight;e.classList.contains("_active")&&(document.body.classList.remove("_lock"),e.classList.remove("_active"),t.classList.remove("_active")),window.scrollTo({top:o,behavior:"smooth"})}}else window.location.href=`${window.location.origin}/dist/html/index.html${c}`;o.preventDefault()}}o.forEach((e=>{e.addEventListener("click",c)}))}})();
+"use strict"
+const burger = document.querySelector('.header-burger');
+const menuBody = document.querySelector('.menu-body');
+if (burger){
+   
+    burger.addEventListener("click", function(e){
+        document.body.classList.toggle('_lock')
+        burger.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    })
+}
+
+
+
+const menuLinks = document.querySelectorAll('.menu-link[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        const goto = menuLink.dataset.goto;  // Доступ к значению атрибута data-goto
+        if (goto) {
+            // Если текущее местоположение не является корневым
+            if (window.location.pathname !== '/' && !window.location.pathname.endsWith('/index.html')) {
+                // Перенаправление на главную страницу с якорем
+                window.location.href = `${window.location.origin}/dist/html/index.html${goto}`;
+            } else {
+                // Если пользователь на главной странице, выполнить прокрутку
+                if (document.querySelector(goto)) {
+                    const gotoBlock = document.querySelector(goto);
+                    const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.scrollY - document.querySelector('header').offsetHeight;
+                   
+                    if (burger.classList.contains('_active')){
+                            document.body.classList.remove('_lock')
+                            burger.classList.remove('_active');
+                            menuBody.classList.remove('_active');
+                    }
+                    
+                    window.scrollTo({
+                        top: gotoBlockValue,
+                        behavior: "smooth"
+                    });
+                }
+            }
+            e.preventDefault();
+        }
+    }
+}
