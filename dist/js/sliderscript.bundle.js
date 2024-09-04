1fa5e2c6d3f72712e4df7dd44df0cb9805ed3360
thegilasband <ataev2808@mail.ru>
2024-09-04 22:15:45 +0300

    Update sliderscript.bundle.js

--- dist/js/sliderscript.bundle.js
+++ dist/js/sliderscript.bundle.js
@@ -1,1 +1,41 @@
-document.querySelector(".swiper-container")&&new Swiper(".image-slider",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0,loop:!0},simulateTouch:!1,slidesPerView:1,autoHeight:window.innerWidth<=1050,autoplay:{delay:5e3,stopOnLastSlide:!1,disableOnInteraction:!1},speed:2e3});
+if (document.querySelector('.swiper-container')){
+    new Swiper(".image-slider", {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            
+            loop: true,
+        },
+        simulateTouch: false,
+        touchRatio: 2,
+        slidesPerView: 1,
+        autoHeight: window.innerWidth <= 1050,
+
+        autoplay: {
+            delay: 5000,
+            stopOnLastSlide: false,
+            disableOnInteraction: false
+        },
+        speed: 2000,
+
+
+        
+    });
+
+    if (window.innerWidth < 812) {
+                swiper.on('touchStart', function () {
+                    swiper.params.speed = 300; // Увеличиваем скорость при ручном скролле
+                });
+
+                swiper.on('touchEnd', function () {
+                    swiper.params.speed = 1000; // Возвращаем скорость к автоскроллу
+                });
+            }
+
+}
+
+
